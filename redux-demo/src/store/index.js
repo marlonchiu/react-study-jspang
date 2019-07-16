4e1083580a4c2772692a2b31e57353b376ec6116
MarlonChiu <knight1898@outlook.com>
2019-07-17 00:10:14 +0800

    redux-thunk 中间件的安装和配置

--- redux-demo/src/store/index.js
+++ redux-demo/src/store/index.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
+import thunk from 'redux-thunk'
+
+// 自定义增强函数  利用compose创造一个增强函数，就相当于建立了一个链式函数
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+    
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    enhancer
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 export default store
