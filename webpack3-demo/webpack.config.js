b79c09db4e32d518ed78dbc0b703b34c4f6db485
MarlonChiu <knight1898@outlook.com>
2019-09-18 00:11:12 +0800

    babel-preset-env 设置独立

--- webpack3-demo/webpack.config.js
+++ webpack3-demo/webpack.config.js
@@ -35,9 +35,9 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
-            options: {
-              presets: ["es2015", "react"]
-            }
+            // options: {
+            //   presets: ["es2015", "react"]
+            // }
           }
         ],
         exclude: /node_modules/
