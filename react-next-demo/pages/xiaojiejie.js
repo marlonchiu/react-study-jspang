f8b4bff9ceb23814211775ab82ee03ce39a13503
marlonchiu <Knight1898@outlook.com>
2019-09-25 00:12:15 +0800

    react-next-demo getInitialProps 阶段请求异步数据

--- react-next-demo/pages/xiaojiejie.js
+++ react-next-demo/pages/xiaojiejie.js
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import { withRouter } from 'next/router'
+import axios from 'axios'
+import { resolve } from 'url';
+
 const Xiaojiejie = ({router}) => {
   return (
     <div>
@@ -7,6 +10,17 @@ const Xiaojiejie = ({router}) => {
       <Link href="/"><a>返回首页</a></Link>
     </div>
   )
+}
+
+Xiaojiejie.getInitialProps = async () => {
+  const promise = new Promise((resolve) => {
+    axios('https://www.easy-mock.com/mock/5cfcce489dc7c36bd6da2c99/xiaojiejie/getList').then(res => {
+      console.log('远程数据结果：', res)
+      resolve(res.data.data)
+    })
+  })
+
+  return await promise
 }
 
 export default withRouter(Xiaojiejie)
