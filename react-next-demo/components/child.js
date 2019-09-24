486839971d8352366fa587a1e424ab484365f3a7
marlonchiu <Knight1898@outlook.com>
2019-09-24 20:32:09 +0800

    react-next-demo 简单初识

---
+++ react-next-demo/components/child.js
@@ -0,0 +1,8 @@
+function Child({ children }) {
+  return (
+    <button>{children}</button>
+  )
+}
+
+export default Child
+// export default ({children})=><button>{children}</button>
---
+++ react-next-demo/pages/index.js
@@ -0,0 +1,11 @@
+import Child from '../components/child'
+function Index() {
+  return (
+    <div>
+      Hello Next.js
+      <Child>Button</Child>
+    </div>
+  )
+}
+
+export default Index
