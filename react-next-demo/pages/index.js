4554b03fc595a76a156b7ab13c7aca945ceae889
marlonchiu <Knight1898@outlook.com>
2019-09-25 00:05:33 +0800

    react-next-demo 路由 6个钩子函数

--- react-next-demo/pages/index.js
+++ react-next-demo/pages/index.js
@@ -3,6 +3,34 @@ import Link from 'next/link'
 import Router from 'next/router'
 
 const Home = () => {
+  // 路由发生变化时
+  Router.events.on('routeChangeStart', (...args) => {
+    console.log('1 -- routeChangeStart->路由开始变化,参数为:', ...args)
+  })
+  // 路由结束变化时
+  Router.events.on('routeChangeComplete', (...args) => {
+    console.log('2 -- routeChangeComplete->路由结束变化,参数为:', ...args)
+  })
+  
+  // 浏览器history触发前
+  Router.events.on('beforeHistoryChange', (...args) => {
+    console.log('3 -- beforeHistoryChange->浏览器history触发前,参数为:', ...args)
+  })
+  // 路由跳转发生错误时  404找不到路由页面不算错误
+  Router.events.on('routeChangeError', (...args) => {
+    console.log('4 -- routeChangeError->路由跳转发生错误时,参数为:', ...args)
+  })
+
+  // hash跳转发生变化时
+  Router.events.on('hashChangeStart', (...args) => {
+    console.log('5 -- hashChangeStart->hash跳转开始变化,参数为:', ...args)
+  })
+  // hash跳转结束变化时
+  Router.events.on('hashChangeComplete', (...args) => {
+    console.log('6 -- hashChangeComplete->hash跳转结束变化,参数为:', ...args)
+  })
+
+
   function goToB() {
     Router.push("/jspangB")
   }
@@ -40,6 +68,9 @@ const Home = () => {
       <Link href={{pathname:'/xiaojiejie', query:{name: '赵敏'}}}><a>选择赵敏</a></Link>
       <br/>
       <button onClick={gotoXiaojiejie}>穆念慈</button>
+      <div>
+        <Link href="#jspang"><a>选JSPang</a></Link>
+      </div>
     </>
   ) 
 }
