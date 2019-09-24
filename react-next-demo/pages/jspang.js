2f3404b6be711f51d582b7bc54c41e12483c6a2c
marlonchiu <Knight1898@outlook.com>
2019-09-25 00:20:34 +0800

    react-next-demo style jsx运用

---
+++ react-next-demo/pages/jspang.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+
+function Jspang() {
+  const [color, setColor] = useState('blue')
+  const changeColor = () => {
+    setColor(
+      color == 'blue' ? 'red' : 'blue'
+    )
+  }
+
+  return (
+    <>
+      <div>技术胖免费前端教程</div>
+      <div className="jspang">技术胖免费前端教程</div>
+      <div><button onClick={changeColor}>改变颜色</button></div>
+      <style jsx>
+        {`
+          div {
+            color: ${color};
+          }
+          .jspang {
+            color: pink;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+export default Jspang
