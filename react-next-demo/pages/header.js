d1d63fc7173ddcbf97138727a734b7511bd8b11c
marlonchiu <Knight1898@outlook.com>
2019-09-25 08:30:38 +0800

    react-next-demo 引入css样式和使用antd UI

--- react-next-demo/pages/header.js
+++ react-next-demo/pages/header.js
@@ -1,14 +1,18 @@
 import Head from 'next/head'
-import Myheader from '../components/myheader'
+// import Myheader from '../components/myheader'
+import {Button} from 'antd'
+// Next.js默认是不支持CSS样式引入的
+import '../static/test.css'
 
 function Header() {
   return (
     <>
-      {/* <Head>
+      <Head>
         <title>技术胖是最胖的！</title>
-      </Head> */}
-      <Myheader>JSPang.com</Myheader>
+      </Head>
+      {/* <Myheader>JSPang.com</Myheader> */}
       <div>自定义head 友好SEO</div>
+      <Button type="primary">Antd Button</Button>
     </>
   )
 }
